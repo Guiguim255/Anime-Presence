a406cfc915bdb48ae7f66cbcd4cfb3a9c8b47c6d
Guiguim <guiguim255@gmail.com>
2020-04-13 16:51:38 +0200

    :bug: Removing "saison s_nb" showing

--- Main.py
+++ Main.py
@@ -4,13 +4,17 @@ from time import sleep, time
 from anime_infos import get_anime_info
 
 
-def generate_state(language_format, translations, variables):
+def generate_state(language_format, translations, variables, removable=None):
     new_str = language_format
     for element in translations:
         new_str = new_str.replace(element, translation[language][element])
 
     for element in variables:
         new_str = new_str.replace(element, infos[element])
+
+    if removable is not None:
+        for element in removable:
+            new_str = new_str.replace(element, "")
 
     return new_str
 
@@ -40,7 +44,7 @@ while True:
     if infos["s_nb"] != "/":
         state = generate_state(l_format, ["saison", "episode"], ["anime_name", "ep_nb", "s_nb"])
     else:
-        state = generate_state(l_format, ["episode"], ["anime_name", "ep_nb"])
+        state = generate_state(l_format, ["episode"], ["anime_name", "ep_nb"], ["saison", "s_nb"])
 
     RPC.update(details=translation[language]["watching an anime"], state=state, large_image=infos["image"], small_image=infos["small_image"],
                start=actual_epoch)
